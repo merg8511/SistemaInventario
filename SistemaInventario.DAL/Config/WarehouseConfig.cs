cdea5658dc250ce5ccdce984e98e41cbbdfe5d83
merg8511 <merg8511@gmail.com>
2023-09-13 14:43:57 -0600

    Configuracion del Proyecto

---
+++ SistemaInventario.DAL/Config/WarehouseConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.DAL.Config
+{
+    public class WarehouseConfig : IEntityTypeConfiguration<Warehouse>
+    {
+        public void Configure(EntityTypeBuilder<Warehouse> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.WHState).IsRequired();
+        }
+    }
+}
---
+++ SistemaInventario.Models/Warehouse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Models
+{
+    public class Warehouse
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [Display(Name = "Bodega")]
+        [MaxLength(60, ErrorMessage = "Nombre debe ser máximo 60 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [Display(Name = "Descripción")]
+        [MaxLength(100, ErrorMessage = "Nombre debe ser máximo 60 caracteres.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Estado es requerido")]
+        [Display(Name = "Estado")]
+        public bool WHState { get; set; }
+    }
+}
